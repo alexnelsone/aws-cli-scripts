6dfbc598d12e8b928001a80c6c6d2cb806ed1de4
Alex Nelson <nelsone@nelsone.io>
2020-05-28 16:05:23 -0600

    added script to get size of s3 contents

---
+++ s3/get_size.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+bucket=$1
+region=$2
+now=$(date +%s)
+aws cloudwatch get-metric-statistics --namespace AWS/S3 --start-time "$(echo "$now - 86400" | bc)" --end-time "$now" --period 86400 --statistics Average --region $region --metric-name BucketSizeBytes --dimensions Name=BucketName,Value="$bucket" Name=StorageType,Value=StandardStorage
